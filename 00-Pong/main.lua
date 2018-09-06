cb2284b3b0e84c10c03c0ea5e3df1cd556123da8
Branko Stankovic <branko.stankovic@gmail.com>
2018-09-06 23:50:10 +0200

    The Serve Update

--- 00-Pong/main.lua
+++ 00-Pong/main.lua
@@ -73,6 +73,9 @@ function love.load()
     player1Score = 0
     player2Score = 0
     
+    -- either going to be 1 or 2; whomever is scored on gets to serve the following turn
+    servingPlayer = 1
+    
     -- initialize our player paddles; make them global so that they can be detected by other functions and modules
     player1 = Paddle(10, 30, 5, 20)
     player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
@@ -88,7 +91,15 @@ end
     Runs every frame, with "dt" passed in, our delta in seconds since the last frame, which LOVE2D supplies us
 ]]
   function love.update(dt)
-    if gameState == 'play' then
+    if gameState == 'serve' then
+        -- before switching to play, initialize ball;s velocity based on player who last scored
+        ball.dy = math.random(-50,50)
+        if servingPlayer == 1 then
+            ball.dx = math.random(140,200)
+        else
+            ball.dx = -math.random(140,200)
+        end
+    elseif gameState == 'play' then
         -- detect ball collision with paddles, reversing dx if true and slightly increasing it, then altering dy based on the position of collision
         if ball:collides(player1) then
             ball.dx = -ball.dx * 1.03
@@ -131,14 +142,14 @@ end
         servingPlayer = 1
         player2Score = player2Score + 1
         ball:reset()
-        gameState = 'start'
+        gameState = 'serve'
     end
     
     if ball.x > VIRTUAL_WIDTH then
         servingPlayer = 2
         player1Score = player1Score + 1
         ball:reset()
-        gameState = 'start'
+        gameState = 'serve'
     end    
     
     -- player 1 movement
@@ -180,12 +191,9 @@ end
     -- if we press enter during the start state of the game, we'll go into play mode; during play mode, the ball will move in a random direction
     elseif key == 'enter' or key == 'return' then
         if gameState == 'start' then
+            gameState = 'serve'
+        elseif gameState == 'serve' then
             gameState = 'play'
-        else
-            gameState = 'start'
-            
-            -- ball's new reset method
-            ball:reset()
         end    
     end
 end    
@@ -203,10 +211,19 @@ function love.draw()
     -- draw different things based on the state of the game
     love.graphics.setFont(smallFont)
     
-    -- draw score on the left and right center of the screen; need to switch font to draw before actually printing
-    love.graphics.setFont(scoreFont)
-    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
-    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+    displayScore()
+    
+    if gameState == 'start' then
+        love.graphics.setFont(smallFont)
+        love.graphics.printf('Welcome to Pong!', 0, 10, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf('Press Enter to begin!', 0, 20, VIRTUAL_WIDTH, 'center')
+    elseif gameState == 'serve' then
+        love.graphics.setFont(smallFont)
+        love.graphics.printf('Player ' .. tostring(servingPlayer) .. "'s serve!", 0, 10, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf('Press Enter to serve!', 0, 20, VIRTUAL_WIDTH, 'center')
+    elseif gameState == 'play' then
+        -- no UI messages to display in play
+    end    
     
     -- render paddles, now using their class's render method
     player1:render()
@@ -232,3 +249,12 @@ function displayFPS()
     love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
 end    
 
+--[[
+    Simply draws the score to the screen
+]]
+function displayScore()
+    -- draw score on the left and right center of the screen; need to switch font to draw before actually printing
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+end    
