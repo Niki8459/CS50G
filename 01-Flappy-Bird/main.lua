c2db5403f06b787c5035e81354e1fbde8f5f2516
Branko Stankovic <branko.stankovic@gmail.com>
2018-09-09 21:54:44 +0200

    The Anti-gravity Update

--- 01-Flappy-Bird/main.lua
+++ 01-Flappy-Bird/main.lua
@@ -56,6 +56,9 @@ function love.load()
             fullscreen = false,
             resizable = true
         })
+    
+    -- initialize input table
+    love.keyboard.keysPressed = {}
 end    
 
 function love.resize(w,h)
@@ -63,8 +66,22 @@ function love.resize(w,h)
 end    
 
 function love.keypressed(key)
-   if key == 'escape' then
+    -- add to our table of keys pressed this frame
+    love.keyboard.keysPressed[key] = true
+    
+    if key == 'escape' then
        love.event.quit() 
+    end    
+end    
+
+--[[
+    New function used to check our global input table for keys we activated during this frame, looked up by their string value
+]]
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
     end    
 end    
 
@@ -76,6 +93,9 @@ function love.update(dt)
     groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
     
     bird:update(dt)
+    
+    -- reset input table
+    love.keyboard.keysPressed = {}
 end    
 
 function love.draw()
