472e476bf7c2593a871054c1f1feadb61ecf9877
Branko Stankovic <branko.stankovic@gmail.com>
2018-09-09 20:25:30 +0200

    The Bird Update

--- 01-Flappy-Bird/main.lua
+++ 01-Flappy-Bird/main.lua
@@ -11,6 +11,12 @@
 -- virtual resolution handling library
 push = require 'push'
 
+-- classic OOP class library
+Class = require 'class'
+
+-- our Bird class
+require 'Bird'
+
 -- physical screen dimensions
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -33,6 +39,9 @@ local GROUND_SCROLL_SPEED = 60
 
 -- point at which we should loop our background back to X 0
 local BACKGROUND_LOOPING_POINT = 413
+
+-- our bird sprite
+local bird = Bird()
 
 function love.load()
     -- initialize our nearest-neighbor filter for preventing bloored images
@@ -77,6 +86,9 @@ function love.draw()
     
     -- draw the ground on top of the background, toward the bottom of the screen, at its negative looping point
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+    
+    -- render our bird to the screen using its own render logic
+    bird:render()
     
     push:finish()
 end    
