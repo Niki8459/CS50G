68222e4551075397e8d79956abc90f8a70ebb6e2
Branko Stankovic <branko.stankovic@gmail.com>
2018-09-26 19:38:45 +0200

    Also the Ugly Way

--- 03-Match3/timer/main.lua
+++ 03-Match3/timer/main.lua
@@ -18,6 +18,18 @@ function love.load()
     currentSecond = 0
     secondTimer = 0
     
+    currentSecond2 = 0
+    secondTimer2 = 0
+    
+    currentSecond3 = 0
+    secondTimer3 = 0
+    
+    currentSecond4 = 0
+    secondTimer4 = 0
+    
+    currentSecond5 = 0
+    secondTimer5 = 0
+    
     love.graphics.setDefaultFilter('nearest', 'nearest')
     
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -43,12 +55,48 @@ function love.update(dt)
     if secondTimer > 1 then
         currentSecond = currentSecond + 1
         secondTimer = secondTimer % 1
+    end
+    
+    secondTimer2 = secondTimer2 + dt
+    
+    if secondTimer2 > 2 then
+        currentSecond2 = currentSecond2 + 1
+        secondTimer2 = secondTimer2 % 2
+    end    
+    
+    secondTimer3 = secondTimer3 + dt
+    
+    if secondTimer3 > 3 then
+        currentSecond3 = currentSecond3 + 1
+        secondTimer3 = secondTimer3 % 3
+    end    
+    
+    secondTimer4 = secondTimer4 + dt
+    
+    if secondTimer4 > 4 then
+        currentSecond4 = currentSecond4 + 1
+        secondTimer4 = secondTimer4 % 4
+    end    
+    
+    secondTimer5 = secondTimer5 + dt
+    
+    if secondTimer5 > 5 then
+        currentSecond5 = currentSecond5 + 1
+        secondTimer5 = secondTimer5 % 5
     end    
 end    
 
 function love.draw()
     push:start()
-    love.graphics.printf('Timer: ' .. tostring(currentSecond) .. ' seconds',
-        0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Timer: ' .. tostring(currentSecond) .. ' seconds (every 1)',
+        0, 68, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Timer: ' .. tostring(currentSecond2) .. ' seconds (every 2)',
+        0, 82, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Timer: ' .. tostring(currentSecond3) .. ' seconds (every 3)',
+        0, 96, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Timer: ' .. tostring(currentSecond4) .. ' seconds (every 4)',
+        0, 110, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Timer: ' .. tostring(currentSecond5) .. ' seconds (every 5)',
+        0, 124, VIRTUAL_WIDTH, 'center')
     push:finish()
 end    
